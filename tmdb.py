fecc1646c3464492106521c330939a1b1713164a
wthered <wthered@gmail.com>
2018-04-20 18:48:19 +0300

    Committed

--- tmdb.py
+++ tmdb.py
@@ -1,5 +1,6 @@
 import json
 
 for line in open("movie_ids_train.json").readlines():
-    data = json.loads(line[:-1])
-    print("INSERT into movies.titles (title_id, title) values (" + str(data['id']) + "," + str(data['']) + str(data['original_title']) + ");")
+	data = json.loads(line[:-1])
+	title = str(data['id'])
+	print("INSERT into movies.titles (title_id, title) values (" + title + ",'" + str(data['original_title']) + "');")
