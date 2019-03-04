e1f991f877b28e555134e475001bd640a5955556
wthered <wthered@gmail.com>
2019-03-04 19:20:51 +0200

    Committed

--- tmdb.py
+++ tmdb.py
@@ -3,4 +3,5 @@ import json
 for line in open("movie_ids_train.json").readlines():
 	data = json.loads(line[:-1])
 	title = str(data['id'])
-	print("INSERT into movies.titles (title_id, title) values (" + title + ",'" + str(data['original_title']) + "');")
+	movie = str(data['original_title'])
+	print("INSERT into movies.titles (title_id, title) values (" + title + ",'" + movie + "');")
