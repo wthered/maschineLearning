b78af7f4332929ad7788eba11adc0bd6b149ad18
wthered <wthered@gmail.com>
2019-03-04 19:35:26 +0200

    Committed

--- svm.py
+++ svm.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
-from matplotlib import style
 import numpy as np
+from matplotlib import style
 
 style.use('ggplot')
 
@@ -12,6 +12,7 @@ class SupportVectorMachine:
 	def __init__(self, visualization=True):
 		self.visualization = visualization
 		self.colors = {1: 'r', -1: 'b'}
+		self.data = None
 		if self.visualization:
 			self.fig = plt.figure()
 			self.ax = self.fig.add_subplot(1, 1, 1)
@@ -19,31 +20,26 @@ class SupportVectorMachine:
 	# train
 	def fit(self, data):
 		self.data = data
-		opt_dict = {}
-		transforms = [
-			[1, -1],
-			[1, 1],
-			[-1, 1],
-			[-1, -1]
-		]
 		pass
 
 	def predict(self, features):
 		# sign( x.w+b )
-		classification = np.sign(np.dot(np.array(features), self.w) + self.b)
-
-		return classification
+		return np.sign(np.dot(np.array(features), self.w) + self.b)
 
 
-data_dict = {
-	-1: np.array([
-		[1, 7],
-		[2, 8],
-		[3, 8],
-	]),
-	1: np.array([
-		[5, 1],
-		[6, -1],
-		[7, 3]
-	])
-}
+if __name__ == '__main__':
+	data_dict = {
+		-1: np.array([
+			[1, 7],
+			[2, 8],
+			[3, 8],
+		]),
+		1: np.array([
+			[5, 1],
+			[6, -1],
+			[7, 3]
+		])
+	}
+	svm = SupportVectorMachine()
+	svm.fit(data=data_dict)
+	print(svm.predict([1, -1]))
